eacac9b9359d276832998b04f4ac5b3a05469d83
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-09-10 09:28:16 +0700

    :)

--- AlvinMusic/helpers/decorators.py
+++ AlvinMusic/helpers/decorators.py
@@ -20,8 +20,8 @@ from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
 
-from AlvinMusicRobot.config import SUDO_USERS
-from AlvinMusicRobot.helpers.admins import get_administrators
+from AlvinMusic.config import SUDO_USERS
+from AlvinMusic.helpers.admins import get_administrators
 
 
 def errors(func: Callable) -> Callable:
