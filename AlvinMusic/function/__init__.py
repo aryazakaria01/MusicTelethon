95e7923812ea47cd6f33e0b5b030487b83980fc7
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-09-10 09:23:00 +0700

    :)

---
+++ AlvinMusic/function/__init__.py
@@ -0,0 +1,5 @@
+from AlvinMusic.function.admins import admins
+from AlvinMusic.function.admins import get
+from AlvinMusic.function.admins import set
+
+__all__ = ["set", "get", "admins"]
--- AlvinMusicRobot/function/__init__.py
+++
@@ -1,5 +0,0 @@
-from AlvinMusicRobot.function.admins import admins
-from AlvinMusicRobot.function.admins import get
-from AlvinMusicRobot.function.admins import set
-
-__all__ = ["set", "get", "admins"]
