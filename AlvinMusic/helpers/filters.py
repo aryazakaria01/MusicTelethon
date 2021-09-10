12ff119cfe75a25668f7ff2ec6de1f17ffd021bc
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-09-10 09:29:44 +0700

    :)

--- AlvinMusic/helpers/filters.py
+++ AlvinMusic/helpers/filters.py
@@ -20,7 +20,7 @@ from typing import Union
 
 from pyrogram import filters
 
-from AlvinMusicRobot.config import COMMAND_PREFIXES
+from AlvinMusic.config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
